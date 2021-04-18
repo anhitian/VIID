aa2f6eae82e30f6fe22311039344524408e07965
qian.he <qian.he.dev@gmail.com>
2021-04-19 02:17:05 +0800

    add viid client

---
+++ viid-client/src/main/java/com/viid/client/config/ViidClientProperties.java
@@ -0,0 +1,20 @@
+package com.viid.client.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * ViidClientProperties
+ *
+ * @author qian.he
+ * @version 0.1.0
+ * @date 2021/4/19
+ * @since 0.1.0
+ **/
+@Data
+@Component
+@ConfigurationProperties(prefix = "viid-client")
+public class ViidClientProperties {
+
+}
